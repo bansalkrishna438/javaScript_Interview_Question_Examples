f6ae33fa8bab2b9f126c23d93a14a9990791633f
bansalkrishna438 <bansalkrishna438@gmail.com>
2025-05-06 23:57:14 +0530

    promise created using asyn await

--- promise.js
+++ promise.js
@@ -1,13 +1,37 @@
-const promise = new Promise((resolve, reject)=>{
-    const arr = [1,2,3]
-    const newArray = arr.map((e)=> e*1)
-    for(let i = 0;i<arr.length;i++){
-        if(newArray[i]%2 === 0){
-            resolve("all numbers are even")
-        }
-        else{
-            reject("arr is not even")
-        }
+// const promise = new Promise((resolve, reject)=>{
+//     const arr = [1,2,3]
+//     const newArray = arr.map((e)=> e*1)
+//     for(let i = 0;i<arr.length;i++){
+//         if(newArray[i]%2 === 0){
+//             resolve("all numbers are even")
+//         }
+//         else{
+//             reject("arr is not even")
+//         }
+//     }
+// });
+// promise.then((value) => console.log(value)).catch((err) => console.log(err));
+
+const promiseTwo = new Promise((resolve,reject) =>{
+    let error = true
+    if(!error){
+        resolve("promise resolve"); 
     }
-});
-promise.then((value) => console.log(value)).catch((err) => console.log(err));
+    else{
+        reject("promise reject")
+    }
+})
+
+async function isPromiseResolve() {
+    try{
+        const response = await promiseTwo
+        console.log(response) 
+    }
+    catch(err){
+        console.log("err");
+        
+        
+    }
+}
+
+isPromiseResolve()
